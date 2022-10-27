cedf3722d147d8635ac1b6daa649054139a1b9ba
oki-aryawan <sandikalakual@gmail.com>
2022-10-27 23:50:05 +0700

    otw final

--- main-tsra2022/main-tsra2022.ino
+++ main-tsra2022/main-tsra2022.ino
@@ -1,24 +1,33 @@
 #include <Wire.h> 
-#define Buzzer 2 //mendefinisikan pin 4 Arduino sebagai pin Output Buzzer
-#define trigPin 9                       //mendefinisikan pin 5 arduino sebagai trigger pin sensor ultrasonic
-#define echoPin 8 //mendefinisikan pin 6 arduino sebagai echoPin sensor ultrasonic
-long duration; // variabel durasi sensor ultrasonic
-int distance = 0; // jarah dalam centimeter
+#define lampu 2 
+#define trigPin1 9
+#define trigPin2 7
+#define echoPin1 8
+#define echoPin2 6
+
+long duration1;
+long duration2; 
+int distance1 = 0;
+int distance2 = 0;
+
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
+
 void setup()
 {
 
 
-Serial.begin(9600); // setting kecepatan pengiriman serial monitor
-pinMode(trigPin, OUTPUT); // setting triggerpin sebagai output
-pinMode(echoPin, INPUT); // setting echopin sebagai Input
-pinMode(Buzzer, OUTPUT); // setting Buzzer sebagai Output
-digitalWrite(Buzzer, HIGH);
+Serial.begin(9600); 
+pinMode(trigPin1, OUTPUT);
+pinMode(trigPin2, OUTPUT);
+pinMode(echoPin1, INPUT); 
+pinMode(echoPin2, INPUT); 
+pinMode(lampu, OUTPUT); 
+digitalWrite(lampu, HIGH);
 delay(1000);
-digitalWrite(Buzzer, LOW);
+digitalWrite(lampu, LOW);
 delay(1000);
 
   mySoftwareSerial.begin(9600);
@@ -31,22 +40,42 @@ delay(1000);
 void loop()
 {
 
-digitalWrite(trigPin, HIGH);// aktifkan sensor ultrasonic
-delayMicroseconds(10); // selama 10 microseconds
-digitalWrite(trigPin, LOW); // matikan sensor ultrasonic
-duration = pulseIn(echoPin, HIGH); // baca rentan waktu dari trigPin High sampai echoPin high
-distance= duration*0.034/2; //konversi selang waktu ke CM
+/*
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance= duration*0.034/2;
+*/
 
-Serial.println(int(distance)); // kirim data jarak ke PC
+distance1 = hitungJarak(trigPin1, echoPin1);
+Serial.print("Sensor1: ");
+Serial.println(int(distance1));
 delay(500);
-if (distance < 20) // Hanya menampilkan jarak jika jaraknya kurang dari 300 cm / 3 meter
+
+if (distance1 < 120) // Hanya menampilkan jarak jika jaraknya kurang dari 300 cm / 3 meter
 {
-            myDFPlayer.volume(15); 
+            myDFPlayer.volume(30); 
         myDFPlayer.play(1);
-        digitalWrite(Buzzer,HIGH); 
+        digitalWrite(lampu,HIGH); 
         delay(1000);
 
-digitalWrite(Buzzer,LOW);
+digitalWrite(lampu,LOW);
 delay(500);
 }
+/*
+distance2 = hitungJarak(trigPin2, echoPin2);
+Serial.print("Sensor2: ");
+Serial.println(int(distance2));
+delay(500);
+*/
+}
+
+int hitungJarak (int trigPin, int echoPin){
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  unsigned long pulseTime = pulseIn(echoPin, HIGH);
+  int distance = pulseTime/58;
+  return distance;
 }
